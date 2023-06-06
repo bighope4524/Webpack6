f6eed0897e03f9ab3def53011e1182ff83d55bd9
Yoshitaka Horimoto <68893878+bighope4524@users.noreply.github.com>
2023-06-06 21:02:04 +0900

    React works good

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|jpg|jpeg)/,
+                test: /\.(png|jpe?g)/,
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name]-[contenthash][ext]',
